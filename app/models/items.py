b6828d90f0ec558adf3364a9bea71d759a5d9eda
Alexander Alemayhu <alexander@alemayhu.com>
2025-05-31 14:42:22 +0100

    Fix: Use typing.Optional for Python 3.9 compatibility

--- app/models/items.py
+++ app/models/items.py
@@ -1,8 +1,10 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
+    description: Optional[str] = None
     price: float
-    tax: float | None = None
+    tax: Optional[float] = None
